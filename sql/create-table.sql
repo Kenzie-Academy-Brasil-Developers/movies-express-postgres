8a2c3669377feb18c04271fb75d55c5c03ed72f5
Ana Carolina <carol.oliva.cruz@gmail.com>
2023-02-06 04:04:41 -0300

    fix: clean code

---
+++ sql/create-table.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE db_movies;
+
+CREATE TABLE if NOT EXISTS "movies" (
+     "id" BIGSERIAL PRIMARY KEY,
+     "name" VARCHAR(50) UNIQUE NOT NULL,
+     "description" TEXT NOT NULL,
+     "duration" INT NOT NULL,
+     "price" INT NOT NULL
+)
+
+
